d3694a1c3258ffa731f8b499d4bd4b5fa69230d0
kuznetsovvdanila <kuznetsovvdanila@gmail.com>
2022-08-01 03:12:46 +0300

    database settings changed

--- currency_project/settings.py
+++ currency_project/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'currency_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'cyrrencydb',
-#         'USER': 'kuznetsovv',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cyrrencydb',
+        'USER': 'kuznetsovv',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
